6ba45c73da9e5ff2b313d5f81c44c0e7dad7e1e8
protex <protex@users.noreply.github.com>
2021-11-29 08:19:55 -0600

    Added node, skhd, yabai

--- home.nix
+++ home.nix
@@ -1,14 +1,19 @@
+let 
+  extraNodePackages = import ./node/default.nix { };
+in
 { config, pkgs, lib, ... }:
-
 with builtins; {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   home = {
+    stateVersion = "21.11";
     username = "pmggio";
     homeDirectory = "/Users/pmggio";
-    stateVersion = "21.11";
 
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
     packages = with pkgs; [
       ripgrep
       tmux
@@ -17,10 +22,14 @@ with builtins; {
       reattach-to-user-namespace
       jq
       hexedit
+      nodejs-12_x
+      nodePackages.node2nix
+      extraNodePackages.lerna
+      yabai
+      skhd
+      sshpass
+      yarn
     ];
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
   };
 
   programs = {
@@ -81,5 +90,55 @@ with builtins; {
   };
 
   home.file.".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.tmux/conf.d/tmux.conf";
+
+  home.file."/Library/LaunchAgents/yabai.plist".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Disabled</key>
+        <false/>
+        <key>Label</key>
+        <string>yabai</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>${config.home.homeDirectory}/.nix-profile/bin/yabai</string>
+        </array>
+        <key>EnvironmentVariables</key>
+        <dict>
+            <key>PATH</key>
+            <string>${config.home.homeDirectory}/.nix-profile/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+        </dict>
+        <key>RunAtLoad</key>
+        <true/>
+    </dict>
+    </plist>
+  '';
+
+  home.file."/Library/LaunchAgents/skhd.plist".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Disabled</key>
+        <false/>
+        <key>Label</key>
+        <string>skhd</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>${config.home.homeDirectory}/.nix-profile/bin/skhd</string>
+        </array>
+        <key>EnvironmentVariables</key>
+        <dict>
+            <key>PATH</key>
+            <string>${config.home.homeDirectory}/.nix-profile/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+        </dict>
+        <key>RunAtLoad</key>
+        <true/>
+    </dict>
+    </plist>
+  '';
+  home.file."/.yabairc".source = ./yabai/.yabairc;
+  home.file."/.skhdrc".source = ./skhd/.skhdrc;
 
 }
