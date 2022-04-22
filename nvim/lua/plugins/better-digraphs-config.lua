286c308b5e0dd810a497d9d0a59aa0723cf99d60
protex <protex@users.noreply.github.com>
2022-04-22 10:44:03 -0500

    Fix better-digraph config

--- nvim/lua/plugins/better-digraphs-config.lua
+++ nvim/lua/plugins/better-digraphs-config.lua
@@ -1,4 +1,4 @@
 local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap('i', '<C-k><C-k>', '<Cmd>lua require"betterdigraphs".digraphs("i")<CR>', opts)
 vim.api.nvim_set_keymap('n', 'r<C-k><C-k>', '<Cmd>lua require"betterdigraphs".digraphs("r")<CR>', opts)
-vim.api.nvim_set_keymap('v', 'r<C-k><C-k>', '<ESC><Cmd>lua require"betterdigraphs".digraphs("r")<CR>', opts)
+vim.api.nvim_set_keymap('v', 'r<C-k><C-k>', '<ESC><Cmd>lua require"betterdigraphs".digraphs("gvr")<CR>', opts)
